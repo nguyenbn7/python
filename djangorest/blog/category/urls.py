7a2a4f80576214e9981f13e3d5bb8dbd69d25a64
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-05-17 22:17:52 +0700

    remove project

--- djangorest/blog/category/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from category.views import get_categories
-
-
-urlpatterns = [path("", get_categories)]
--- djangorest/blog/category/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpRequest
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-# Create your views here.
-@api_view(["GET"])
-def get_categories(request: HttpRequest):
-    return Response({"categories": []})
